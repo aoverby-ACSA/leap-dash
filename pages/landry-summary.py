6362fd1b49801c24f2a044c3d5166c4b48773306
Anton Overby <aoverby.devworks@gmail.com>
2023-08-15 10:43:35 -0500

    Summary metrics on each tab, expanded=True on first tab, False on subsequent tabs

--- pages/landry-summary.py
+++ pages/landry-summary.py
@@ -103,6 +103,43 @@ with tab2:
         st.markdown(f"##### Date Range: {yr1}")
     else:
         st.markdown(f"##### Date Range: {yr1} - {yr2}")
+        
+    with st.expander(label='**Summary Metrics**', expanded=False):
+        st.markdown("### Summary Metrics:")
+        col1, col2, col3, col4, col5 = st.columns(5)
+        tot_testers = int(landry[f"{subj_choice}ScaleScore"].count())
+        with col1:
+            metric_card(box_color=(255,255,255), metric= tot_testers, caption='Total Testers', fa_icon_name='fa-solid fa-graduation-cap')   
+        median = round(landry[f"{subj_choice}ScaleScore"].median())
+        with col2:
+            metric_card(metric=median, caption='Median SS', fa_icon_name='fa-solid fa-scale-balanced')    
+        stdev = round(landry[f"{subj_choice}ScaleScore"].std(ddof=1))
+        with col3:
+            metric_card(metric=stdev, caption='Std Dev', fa_icon_name='fa-solid fa-arrows-left-right')
+        range_high = median + stdev
+        with col4:
+            metric_card(metric=range_high, caption='SS Range (High)', fa_icon_name='fa-solid fa-arrow-up')   
+        range_low = median - stdev
+        with col5:
+            metric_card(metric=range_low, caption='SS Range (Low)', fa_icon_name='fa-solid fa-arrow-down')
+        
+        col1, col2, col3, col4, col5 = st.columns(5)
+        num_unsat = landry[f"{subj_choice}AchievementLevel"].str.match('Unsatisfactory').sum()
+        with col1:
+            metric_card(metric=num_unsat, caption="Unsatisfactory", fa_icon_name='', box_color=(220,20,60), font_color=(255,255,255))   
+        num_apbasic = landry[f"{subj_choice}AchievementLevel"].str.match('Approaching Basic').sum()     
+        with col2:
+            metric_card(metric=num_apbasic, caption="App Basic", fa_icon_name='', box_color=(255,165,0), font_color=(255,255,255))   
+        num_basic = landry[f"{subj_choice}AchievementLevel"].str.match('Basic').sum()    
+        with col3:
+            metric_card(metric=num_basic, caption="Basic", fa_icon_name='', box_color=(0,128,0), font_color=(255,255,255))
+        num_mastery = landry[f"{subj_choice}AchievementLevel"].str.match('Mastery').sum()    
+        with col4:
+            metric_card(metric=num_mastery, caption="Mastery", fa_icon_name='', box_color=(144,238,144), font_color=(255,255,255))    
+        num_adv = landry[f"{subj_choice}AchievementLevel"].str.match('Advanced').sum()    
+        with col5:
+            metric_card(metric=num_adv, caption="Advanced", fa_icon_name='', box_color=(0,255,0), font_color=(255,255,255))
+    
     ach_by_yr_line(landry, subj_choice)
     
 with tab3:
@@ -111,6 +148,43 @@ with tab3:
         st.markdown(f"##### Date Range: {yr1}")
     else:
         st.markdown(f"##### Date Range: {yr1} - {yr2}")
+        
+    with st.expander(label='**Summary Metrics**', expanded=False):
+        st.markdown("### Summary Metrics:")
+        col1, col2, col3, col4, col5 = st.columns(5)
+        tot_testers = int(landry[f"{subj_choice}ScaleScore"].count())
+        with col1:
+            metric_card(box_color=(255,255,255), metric= tot_testers, caption='Total Testers', fa_icon_name='fa-solid fa-graduation-cap')   
+        median = round(landry[f"{subj_choice}ScaleScore"].median())
+        with col2:
+            metric_card(metric=median, caption='Median SS', fa_icon_name='fa-solid fa-scale-balanced')    
+        stdev = round(landry[f"{subj_choice}ScaleScore"].std(ddof=1))
+        with col3:
+            metric_card(metric=stdev, caption='Std Dev', fa_icon_name='fa-solid fa-arrows-left-right')
+        range_high = median + stdev
+        with col4:
+            metric_card(metric=range_high, caption='SS Range (High)', fa_icon_name='fa-solid fa-arrow-up')   
+        range_low = median - stdev
+        with col5:
+            metric_card(metric=range_low, caption='SS Range (Low)', fa_icon_name='fa-solid fa-arrow-down')
+        
+        col1, col2, col3, col4, col5 = st.columns(5)
+        num_unsat = landry[f"{subj_choice}AchievementLevel"].str.match('Unsatisfactory').sum()
+        with col1:
+            metric_card(metric=num_unsat, caption="Unsatisfactory", fa_icon_name='', box_color=(220,20,60), font_color=(255,255,255))   
+        num_apbasic = landry[f"{subj_choice}AchievementLevel"].str.match('Approaching Basic').sum()     
+        with col2:
+            metric_card(metric=num_apbasic, caption="App Basic", fa_icon_name='', box_color=(255,165,0), font_color=(255,255,255))   
+        num_basic = landry[f"{subj_choice}AchievementLevel"].str.match('Basic').sum()    
+        with col3:
+            metric_card(metric=num_basic, caption="Basic", fa_icon_name='', box_color=(0,128,0), font_color=(255,255,255))
+        num_mastery = landry[f"{subj_choice}AchievementLevel"].str.match('Mastery').sum()    
+        with col4:
+            metric_card(metric=num_mastery, caption="Mastery", fa_icon_name='', box_color=(144,238,144), font_color=(255,255,255))    
+        num_adv = landry[f"{subj_choice}AchievementLevel"].str.match('Advanced').sum()    
+        with col5:
+            metric_card(metric=num_adv, caption="Advanced", fa_icon_name='', box_color=(0,255,0), font_color=(255,255,255))
+    
     scale_score_distrib(landry, subj_choice)
 
 
